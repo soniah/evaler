c2d1ed8fff716a3895db677b117197792ec34702
Sonia Hamilton <sonia@snowfrog.net>
2012-10-24 14:45:01 +1100

    update doco - supports ** exponent

--- evaler.go
+++ evaler.go
@@ -3,7 +3,7 @@
 // Evaler uses Dijkstra's Shunting Yard algorithm [1] to convert an infix
 // expression to postfix/RPN format [2], then evaluates the RPN expression.
 //
-// The operators supported are: + - * / and parentheses ().
+// The operators supported are: + - * / ** and parentheses ().
 //
 // [1] http://en.wikipedia.org/wiki/Shunting-yard_algorithm
 // [2] http://en.wikipedia.org/wiki/Reverse_Polish_notation
